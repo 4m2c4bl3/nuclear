8ba117c9943df9b33c7ad817ed95624f065360bf
am1cable <tegan@octospiracy.com>
2015-01-20 20:02:03 -0500

    playerStats & targetState
    put them in, framework

--- Assets/Scripts/playerStats.cs
+++ Assets/Scripts/playerStats.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class hurglblrt : MonoBehaviour {
+public class playerStats : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/targetState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class targetState : MonoBehaviour {
+
+    public bool red;
+    public bool orange;
+    public bool yellow;
+    public bool green;
+    public bool blue;
+    public bool purple;
+
+    void redActive () 
+    {
+    //player.life -1
+    //player.respawn(respawnlocation)
+    }
+
+    void orangeActive()
+    {
+        //something else
+    }
+
+    void yellowActive ()
+    {
+        //etc
+    }
+
+}
