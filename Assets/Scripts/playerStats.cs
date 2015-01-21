833c2f3b17b0d64cf4965b0ee71898d5e0931c99
am1cable <tegan@octospiracy.com>
2015-01-20 20:11:14 -0500

    quicikupdated

--- Assets/Scripts/playerStats.cs
+++ Assets/Scripts/playerStats.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class playerStats : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    int Lives = 3;
+    bool _controlState;
+    
+    bool controlState 
+    {
+        get
+        {
+            return _controlState;
+        }
+
+        set
+        {
+            if (_controlState != value)
+            {
+                if (_controlState == true)
+                {
+                    //let player controls be turned back on
+                }
+                if (_controlState == false)
+                {
+                    //disable player controls from working
+                }
+            }
+            else
+            {
+
+            }
+
+            _controlState = value;
+        }
+
+    }
+
 }
