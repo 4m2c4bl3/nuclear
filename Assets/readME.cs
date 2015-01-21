c3a40ade27b2060fc3b45bb3f0522fc8250bd71f
am1cable <tegan@octospiracy.com>
2015-01-21 10:09:10 -0500

    (added a readme)

---
+++ Assets/readME.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class readME : MonoBehaviour {
+
+	/*      Rows are organized from left to right, so you can 
+     *      'see' what orbs should be connected to each other 
+     *      via hierarchy pannel. Row 1 = top row, 2 = row 
+     *      below, etc.
+     * 
+     *      If you make any major changes or move things around 
+     *      please note in here so things don't get lost or mixed up.
+     *      Thanks! -- Tegan    
+     */
+
+     }
