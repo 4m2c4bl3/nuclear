195f814b0abfed5758b00d67dfbf753257bab2ef
am1cable <tegan@octospiracy.com>
2015-02-03 19:57:57 -0500

    workingCamera
    :D

--- Assets/Scripts/camMove.cs
+++ Assets/Scripts/camMove.cs
@@ -6,14 +6,27 @@ public class camMove : MonoBehaviour {
     Transform basePos;
     public Transform target;
     public float smooth = 5.0f;
+    bool targetSet = false;
 
     void Start()
     {
-        basePos = gameObject.transform;
+        transform.position = new Vector3 (transform.position.x, target.position.y, target.position.z - 10);
+    }
+    
+    void setCam()
+    {
+        target = GameObject.FindGameObjectWithTag("Active").transform;
+        basePos = target;
+        targetSet = true;
+
     }
 
     void Update()
     {
+        if (targetSet == false)
+        {
+            setCam();
+        }
         target = GameObject.FindGameObjectWithTag("Active").transform;
         Vector3 targetView = new Vector3(target.position.x, basePos.position.y, target.position.z - 10);
            transform.position = Vector3.Lerp(
